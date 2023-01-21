c4b276fb039a75320075c84b4e560cedc9ac5519
llKatzll <bluedia0113@gmail.com>
2023-01-21 23:53:29 +0900

    아플레이어움직이는거만들었고요달리는거만들었고요첫번째콤보평타만들었고요두번째콤보평타는좌클2번후뜸들인뒤1번으로만들었고요이펙트넣었고요이펙트색변경했고요애니매이션조정했고요카메라구현했고요
    네.

---
+++ Assets/Scripts/AttackAnimation.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimation : StateMachineBehaviour //MonoBehaviour
+{
+
+    private PlayerMove parent;
+    public GameObject effect; //이펙트의 원본
+
+    public enum AttackType
+    {
+        A,
+        B,
+        C1,
+        C2,
+        D,
+        E,
+    }
+
+    public AttackType attackType;
+    private int frame = 0;
+
+
+    private void Awake()
+    {
+        parent = GameObject.FindGameObjectWithTag("LocalPlayer").GetComponent<PlayerMove>(); //PlayerInpur for문으로 AttackAnimation에 할당하는 방법으로 바꿀예정
+
+        switch (attackType) // 스킬 위치값 벡터가 위치 쿼터니언이 로테이션
+        {
+            case AttackType.A:
+                effect = Instantiate(effect, new Vector3(-0.0307480991f, 1.2700001f, 0.189999998f), Quaternion.Euler(30, -100, -200), parent.transform);
+                break;
+            case AttackType.B:
+                effect = Instantiate(effect, new Vector3(0.19406724f, 1.10295975f, -0.0782125294f), Quaternion.Euler(86.5657806f, 87.8785095f, 66.9312592f), parent.transform);
+                effect.transform.localScale = new Vector3(0.800000012f, 0.800000012f, 1);
+                break;
+            case AttackType.C1:
+                effect = Instantiate(effect, new Vector3(0, 1, 1.09f), Quaternion.Euler(0, 0, 0), parent.transform);
+                effect.transform.localScale = new Vector3(1.57f, 1.57f, 1);
+                break;
+            case AttackType.C2:
+                effect = Instantiate(effect, new Vector3(0, 1, 0), Quaternion.Euler(0, 90, 90), parent.transform);
+                break;
+            case AttackType.D:
+                effect = Instantiate(effect, new Vector3(0, 1, 0.369999886f), Quaternion.Euler(0.331470579f, 358.565155f, 5.0682826f), parent.transform);
+                effect.transform.localScale = new Vector3(1.20000005f, 0.800000012f, 1);
+                break;
+            case AttackType.E:
+                effect = Instantiate(effect, new Vector3(0.0774749517f, 0.974506676f, -0.0287958607f), Quaternion.Euler(351.56955f, 185.288879f, 101.211784f), parent.transform);
+                effect.transform.localScale = new Vector3(0.100000001f, 0.100000001f, 1);
+                break;
+        }
+        //                   원본  , 위치        , 로테이션회전값,            부모
+        effect.SetActive(false);
+    }
+
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        frame = 0;
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (attackType == AttackType.C1)
+        {
+            animator.SetBool("isThreeCombo", false);
+        }
+        effect.SetActive(false);
+        animator.SetBool("isAttacking", false);
+        base.OnStateExit(animator, stateInfo, layerIndex);
+    }
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) //애니메이션 한 프레임마다 업데이트
+    {
+        frame++;
+        switch (attackType)
+        {
+            case AttackType.A:
+                if (stateInfo.normalizedTime < .2f)
+                {
+                    animator.SetBool("isAttacking", false);
+                }
+                if (frame == 10)
+                {
+                    effect.SetActive(true);
+                }
+                break;
+            case AttackType.B:
+                if (stateInfo.normalizedTime < .2f)
+                {
+                    animator.SetBool("isAttacking", false);
+                }
+                if (frame == 10)
+                {
+                    effect.SetActive(true);
+                }
+                break;
+            case AttackType.C1:
+                if (stateInfo.normalizedTime < .2f)
+                {
+                    animator.SetBool("isAttacking", false);
+                }
+                if (frame == 10)
+                {
+                    effect.SetActive(true);
+                }
+                break;
+            case AttackType.C2:
+                if (stateInfo.normalizedTime < .1f)
+                {
+                    animator.SetBool("isAttacking", false);
+                }
+                if (frame == 10)
+                {
+                    effect.SetActive(true);
+                }
+                break;
+            case AttackType.D:
+                if (stateInfo.normalizedTime < .1f)
+                {
+                    animator.SetBool("isAttacking", false);
+                }
+                if (frame == 10)
+                {
+                    effect.SetActive(true);
+                }
+                break;
+            case AttackType.E:
+                if (stateInfo.normalizedTime < .2f)
+                {
+                    animator.SetBool("isAttacking", false);
+                }
+                if (frame == 10)
+                {
+                    effect.SetActive(true);
+                }
+                break;
+        }
+
+
+
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    #region Private Field
+    CharacterController controller;
+    float gravity;
+    Animator animator;
+    public float runspeed;
+    long lastForward; //마지막으로 W를 눌렀던 시간
+    #endregion
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetInteger("Horizontal", (int)Input.GetAxisRaw("Horizontal"));
+        animator.SetInteger("Vertical", (int)Input.GetAxisRaw("Vertical"));
+
+        if ((!animator.GetNextAnimatorStateInfo(0).IsTag("Attack") && !animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) && //공격중이거나
+             (!animator.GetNextAnimatorStateInfo(0).IsTag("Guard") && !animator.GetCurrentAnimatorStateInfo(0).IsTag("Guard")))
+        {
+            Vector3 dir = (Vector3.right * speed * Time.deltaTime * Input.GetAxisRaw("Horizontal") * (animator.GetBool("isRunning") ? runspeed : 1)) + //가로
+            (Vector3.forward * speed * Time.deltaTime * Input.GetAxisRaw("Vertical") * (animator.GetBool("isRunning") ? runspeed : 1)) + //세로
+            Vector3.up * gravity; //위아래
+            controller.Move(dir);
+        }
+       
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (DateTimeOffset.Now.ToUnixTimeMilliseconds() - lastForward <= 300)
+            {
+                animator.SetBool("isRunning", true);
+            }
+            lastForward = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        }
+        if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (controller.isGrounded)
+        {
+            gravity = -.1f; //중력이 특정한 힘에서 고정
+        }
+        else
+        {
+            gravity -= .05f; //한 프레임당 중력에 0.1이라는 힘을 더해준다
+        }
+
+        if (Input.GetMouseButtonDown(0)) //좌클릭을 누르는 경우
+        {
+            if(animator.GetCurrentAnimatorStateInfo(0).shortNameHash == Animator.StringToHash("2") && !animator.GetBool("isAttacking"))
+            {
+                if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.6)
+                {
+                    animator.SetBool("isThreeCombo", true);
+                }
+            }
+            Debug.Log("Clicked");
+            if (!animator.GetBool("isAttacking") && !animator.GetNextAnimatorStateInfo(0).IsTag("Attack") && !animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+            {
+                animator.CrossFade("AttackStart", .25f,0);
+                animator.CrossFade("AttackStart", .25f,1);
+            }
+            animator.SetBool("isAttacking", true);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F)) //방어모드
+        {
+            if (!animator.GetBool("isGuarding") && !animator.GetNextAnimatorStateInfo(0).IsTag("Guard") && !animator.GetCurrentAnimatorStateInfo(0).IsTag("Guard"))
+            {
+                animator.CrossFade("GuardStart", .25f, 0);
+            }
+            animator.SetBool("isGuarding", true);
+        }
+        if (Input.GetKeyUp(KeyCode.F)) //방어 모드 (때는데 걸리는 시간 검사 예정)
+        {
+            animator.SetBool("isGuarding", false);
+        }
+    }
+}
